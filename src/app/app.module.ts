e148edf0f29a5c01edb29d95e1933791acd851f3
jardel88 <jardelsilvagomes@hotmail.com>
2025-04-25 11:16:18 -0300

    remocao info

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,6 @@ import { HeaderComponent } from './components/header/header.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { PanelComponent } from './components/panel/panel.component';
 import { ArtboardComponent } from './components/artboard/artboard.component';
-import { InfoBarComponent } from './components/info-bar/info-bar.component';
 
 @NgModule({
   declarations: [
@@ -16,8 +15,7 @@ import { InfoBarComponent } from './components/info-bar/info-bar.component';
     HeaderComponent,
     SidebarComponent,
     PanelComponent,
-    ArtboardComponent,
-    InfoBarComponent
+    ArtboardComponent
   ],
   imports: [
     BrowserModule,
