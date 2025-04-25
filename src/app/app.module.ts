19f473d47d1aafd51dd3790bcafae3ea06c43f4f
jardel88 <jardelsilvagomes@hotmail.com>
2025-04-25 10:01:44 -0300

    upload inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { PanelComponent } from './components/panel/panel.component';
+import { ArtboardComponent } from './components/artboard/artboard.component';
+import { InfoBarComponent } from './components/info-bar/info-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    PanelComponent,
+    ArtboardComponent,
+    InfoBarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/artboard/artboard.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-artboard',
+  templateUrl: './artboard.component.html',
+  styleUrl: './artboard.component.css'
+})
+export class ArtboardComponent {
+
+}
